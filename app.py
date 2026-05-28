488590c315c575e2ddf5877d75061d13f0bdc207
Thein Lwin Oo <uthein1969@gmail.com>
2026-05-28 10:51:06 +0000

    step2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pytz
 from datetime import datetime
+from supabase import create_client
 
 st.set_page_config(layout="wide")
 
@@ -9,7 +10,7 @@ yangon_tz = pytz.timezone("Asia/Yangon")
 now_yangon = datetime.now(yangon_tz)
 
 # ---------------- TEST ----------------
-st.title("🧼 STEP 1 SUCCESS")
-st.success("Imports working")
+st.title("🧼 STEP 2 SUCCESS")
+st.success("Supabase import working")
 
 st.write(now_yangon.strftime("%Y-%m-%d %H:%M:%S"))
