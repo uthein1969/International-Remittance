40551f891eca96fcf521c7c634c655a132ae83f8
Thein Lwin Oo <uthein1969@gmail.com>
2026-05-28 10:49:52 +0000

    step1

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pytz
+from datetime import datetime
 
 st.set_page_config(layout="wide")
 
-st.title("🧼 CLEAN RESET MODE")
-st.success("App is now stable (no DB, no network)")
-st.write("If you see this → Streamlit is OK")
+# ---------------- TIME ----------------
+yangon_tz = pytz.timezone("Asia/Yangon")
+now_yangon = datetime.now(yangon_tz)
+
+# ---------------- TEST ----------------
+st.title("🧼 STEP 1 SUCCESS")
+st.success("Imports working")
+
+st.write(now_yangon.strftime("%Y-%m-%d %H:%M:%S"))
